fb671aee7f67a580cfd116449856811829a245f2
rich <dankwarrior6900@gmail.com>
2023-08-10 17:54:29 -0700

    Do not use switch statements for evaluting operators

--- app.js
+++ app.js
@@ -1,3 +1,38 @@
 "use strict"
 
-console.log('hi');
+function addition(a, b) {
+    return a + b
+};
+
+function subtraction(a, b) {
+    return a - b
+};
+
+function multiplication(a, b) {
+    return a * b
+};
+
+function division(a, b) {
+    return a / b
+};
+
+function operate(num1, num2, sign){
+    switch (sign) {
+        case '+':
+            addition(num1, num2);
+            break;
+        case '-':
+            subtraction(num1, num2);
+            break;
+        case '*':
+            multiplication(num1, num2);
+            break;
+        case '/':
+            division(num1, num2);
+            break;
+    }
+}
+
+let firstNum = 1;
+let secondNum = 1;
+let operator = '+';
