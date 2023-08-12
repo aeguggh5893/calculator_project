d2a92c52c98986e3c57a6f08093a5217cadbd1be
rich <dankwarrior6900@gmail.com>
2023-08-11 22:30:03 -0700

    Change all the files to the video tutorial, specifically the html and css files. Set up the UI overall

--- app.js
+++ app.js
@@ -1,42 +1,5 @@
 "use strict"
 
-function addition(a, b) {
-    return a + b
-};
-
-function subtraction(a, b) {
-    return a - b
-};
-
-function multiplication(a, b) {
-    return a * b
-};
-
-function division(a, b) {
-    return a / b
-};
-
-function operate(num1, num2, operation) {
-    if (operation == '+') {
-        return addition(num1, num2);
-    };
-
-    if (operation == '-') {
-        return subtraction(num1, num2);
-    };
-
-    if (operation == '*') {
-        return multiplication(num1, num2);
-    };
-
-    if (operation == '/') {
-        return division(num1, num2);
-    };
-}
-
-// operate(firstNum, secondNum, operator)
-
-
 
 
 
