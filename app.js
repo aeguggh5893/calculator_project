ce4257168c4d311dc3d67ee85b8317a9feb1272b
rich <dankwarrior6900@gmail.com>
2023-08-13 17:17:12 -0700

    Add click event listeners and console logs

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 "use strict"
 
+let calcContainerEle = document.querySelector('#calculator-container');
 
+calcContainerEle.addEventListener('click', e => onButtonClick(e));
+
+
+
+
+function onButtonClick(e) {
+    console.log('clicked', e.target.innerHTML)
+}
 
 
