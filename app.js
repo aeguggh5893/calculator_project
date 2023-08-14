29329c972ae7ee21cc4fbbd11acbaa314463653f
rich <dankwarrior6900@gmail.com>
2023-08-13 17:31:58 -0700

    Create indivdual functions to handle click events

--- app.js
+++ app.js
@@ -4,11 +4,69 @@ let calcContainerEle = document.querySelector('#calculator-container');
 
 calcContainerEle.addEventListener('click', e => onButtonClick(e));
 
+function doNumber(num) {
+    console.log('num', num);
+}
 
+function doOperation(operation) {
+    console.log('op', operation);
+}
 
+function reset() {
+    console.log('reset');
+}
+
+function makePosNeg() {
+    console.log('makeposneg');
+}
+
+function makeDecimal() {
+    console.log('makeDecimal');
+}
+
+function doEqual() {
+    console.log('equal');
+}
 
 function onButtonClick(e) {
-    console.log('clicked', e.target.innerHTML)
+    let btnClickedValue = e.target.innerHTML;
+
+    if (Number.isInteger(parseInt(btnClickedValue))) {
+        doNumber(btnClickedValue);
+        return
+    }
+
+    switch(btnClickedValue) {
+        case 'Clear': 
+            reset()
+            break;
+        case '+/-': 
+            makePosNeg()
+            break;
+        case '/': 
+            doOperation('/')
+            break;
+        case 'x': 
+            doOperation('x')
+            break;
+        case '+': 
+            doOperation('+')
+            break;
+        case '-': 
+            doOperation('-')
+            break;
+        case '.': 
+            makeDecimal()
+            break;
+        case '=': 
+            doEqual()
+            break;
+        default:
+            break;
+    }
+
 }
+
+
 
 
