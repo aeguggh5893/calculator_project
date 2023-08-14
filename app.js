c3e8f692878f6ab37ad621f2aafa3d3d7154b08c
rich <dankwarrior6900@gmail.com>
2023-08-13 17:55:49 -0700

    A lot of errors in this tutorial, will switch to another one

--- app.js
+++ app.js
@@ -4,12 +4,50 @@ let calcContainerEle = document.querySelector('#calculator-container');
 
 calcContainerEle.addEventListener('click', e => onButtonClick(e));
 
+let resultEle = document.querySelector('#result');
+
+let curOperation, curNumber1, curNumber2;
+
+function compute(num1, num2, op) {
+    let n1 = Number(num1);
+    let n2 = Number(num2);
+    let result;
+
+    if (op === '+') {
+        result = n1 + n2;
+    } else if (op === '-') {
+        result = n1 - n2;
+    } else if (op === '/') {
+        result = n1 / n2;
+    } else {
+        result = n1 * n2;
+    }
+    return result
+}
+
+
 function doNumber(num) {
-    console.log('num', num);
+    let newNum;
+
+    if (curOperation) {
+        curNumber2 = curNumber2 ? `${curNumber2}${num}` : num;
+        newNum = curNumber2;
+    } else {
+        curNumber1 = curNumber1 ? `${curNumber1}${num}` : num;
+        newNum = curNumber1;
+    }
+
+    resultEle.innerHTML = newNum;
 }
 
 function doOperation(operation) {
-    console.log('op', operation);
+    if (curNumber2) {
+        let result = compute(curNumber1, curNumber2, curOperation);
+        curNumber1 = result;
+        curNumber2 = 0;
+        resultEle.innerHTML = result
+
+    }
 }
 
 function reset() {
@@ -25,7 +63,10 @@ function makeDecimal() {
 }
 
 function doEqual() {
-    console.log('equal');
+    if (curNumber1 && curNumber2 && curOperation) {
+        let result = compute(curNumber1, curNumber2, curOperation);
+        resultEle.innerHTML = result;
+    }
 }
 
 function onButtonClick(e) {
